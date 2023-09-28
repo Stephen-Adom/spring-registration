39092f8b1f36c034980a4f9283c9aacac39ef4a3
Stephen Adom Addae <stephen.alaska94@hotmail.com>
2023-09-28 22:19:00 +0000

    remove unused import

--- src/main/java/com/alaska/securitylearn/controller/AuthenticationController.java
+++ src/main/java/com/alaska/securitylearn/controller/AuthenticationController.java
@@ -1,12 +1,10 @@
 package com.alaska.securitylearn.controller;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
